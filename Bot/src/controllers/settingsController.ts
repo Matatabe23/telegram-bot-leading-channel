a81b458e1c2103b9d5897667a92124e7b6e85478
Qugor <blackgocha12@gmail.com>
2024-05-09 22:07:45 +0300

    Сделал инициализацию базы данных при запуске контейнера, а так же указал последовательность запуска контейнеров

--- Bot/src/controllers/settingsController.ts
+++ Bot/src/controllers/settingsController.ts
@@ -1,17 +1,17 @@
-import { regularPublicationTime } from '../models/models.js';
+// import { regularPublicationTime } from '../models/models.js';
 import { Request, Response } from 'express';
 
 
 class AdministratorController {
   async addingPublicationTime(req: Request, res: Response) {
-    const { hour, minute } = req.body
-    if (hour && minute) return res.status(404).send('Не корректные данные');
-    const newRegularPublicationTime = await regularPublicationTime.create({
-      hour: hour,
-      minute: minute
-    });
+    // const { hour, minute } = req.body
+    // if (hour && minute) return res.status(404).send('Не корректные данные');
+    // const newRegularPublicationTime = await regularPublicationTime.create({
+    //   hour: hour,
+    //   minute: minute
+    // });
 
-    return res.json(newRegularPublicationTime);
+    // return res.json(newRegularPublicationTime);
   }
 
 }
--- Bot/src/routerBot/index.ts
+++ Bot/src/routerBot/index.ts
@@ -2,18 +2,11 @@ import TelegramBot from 'node-telegram-bot-api'
 import sendMessageAtScheduledTime from './sendMessageAtScheduledTime.js'
 import { scheduleFunctionExecution } from '../service/callCertainTime-service.js'
 import { TELEGRAM_BOT_API_TOKEN } from '../const/constENV.js'
-import { regularPublicationTime } from '../models/models.js'
+import {publishTime} from '../const/const.js'
 
 export const bot = new TelegramBot(TELEGRAM_BOT_API_TOKEN, { polling: true })
 
-
-const allRegularPublicationTimes = await regularPublicationTime.findAll();
-const blocks = allRegularPublicationTimes.map((time:any) => ({
-    hour: time.hour,
-    minute: time.minute
-}));
-
-scheduleFunctionExecution(sendMessageAtScheduledTime, blocks)
+scheduleFunctionExecution(sendMessageAtScheduledTime, publishTime)
 
 export default bot;
 
