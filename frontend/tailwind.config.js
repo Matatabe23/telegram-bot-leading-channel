8b553ee357ee063ea3923558fa7969f7bba9d5a0
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2025-08-13 21:20:04 +0300

    поправил ssl

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,67 +1,17 @@
 // tailwind.config.js
-import defaultTheme from 'tailwindcss/defaultTheme';
 import gridAreas from '@savvywombat/tailwindcss-grid-areas';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    screens: {
-      sm: '320px',
-      m: '525px',
-      md: '768px',
-      lg: '1366px',
-      xl: '1920px',
-    },
-    extend: {
-      fontFamily: {
-        body: ['Montserrat', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        inherit: 'inherit',
-      },
-      fontSize: {
-        '2xs': ['10px', '13px'],
-      },
-      maxWidth: {
-        '1/2': '50%',
-      },
-      spacing: {
-        15: '60px',
-      },
-      borderRadius: {
-        '2.5xl': '20px',
-        2.5: '10px',
-        '4xl': '32px',
-      },
-      gridTemplateAreas: {
-        layout: ['advertisement', 'header', 'main', 'footer'],
-      },
-      gridTemplateRows: {
-        aa1: 'repeat(2, auto) 1fr',
-        a1: 'auto 1fr',
-        '1a': '1fr auto',
-        '1a1': '1fr auto 1fr',
-        a11: 'auto 1fr 1fr',
-        '11a': '1fr 1fr auto',
-        a1a: 'auto 1fr auto',
-        '1aa': '1fr repeat(2, auto)',
-        a1aa: 'auto 1fr repeat(2, auto)',
-        full: '100%',
-      },
-      gridTemplateColumns: {
-        aa1: 'repeat(2, auto) 1fr',
-        a1: 'auto 1fr',
-        '1a': '1fr auto',
-        '1a1': '1fr auto 1fr',
-        a11: 'auto 1fr 1fr',
-        '11a': '1fr 1fr auto',
-        a1a: 'auto 1fr auto',
-        '1aa': '1fr repeat(2, auto)',
-        a1aa: 'auto 1fr repeat(2, auto)',
-        full: '100%',
-      },
-    },
-  },
-  plugins: [gridAreas],
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		screens: {
+			sm: '320px',
+			m: '525px',
+			md: '768px',
+			lg: '1366px',
+			xl: '1920px'
+		}
+	},
+	plugins: [gridAreas]
 };
